1adc84a282639123866e6e0990d9b7466ec572af
Ryan Bowman <rymbowman@gmail.com>
2025-02-24 12:18:32 -0500

    Style PrimaryBtn component

--- Checkbox-Frontend/src/components/PrimaryBtn.jsx
+++ Checkbox-Frontend/src/components/PrimaryBtn.jsx
@@ -1,11 +1,26 @@
-import { Button } from "@mui/material";
+import { Button, styled } from "@mui/material";
 import PropTypes from "prop-types";
+
+const MainButton = styled(Button)({
+  backgroundColor: "#8FBC8F", // Mint green color
+  color: "#fff",
+  "&:hover": {
+    backgroundColor: "#76c776", // Darker mint green on hover
+  },
+  padding: "10px 20px",
+  fontSize: "16px",
+  width: "150px",
+  borderRadius: "8px",
+  textTransform: "none",
+  boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.1)",
+  border: "none",
+});
 
 const PrimaryBtn = ({ buttonText, value, onClick }) => {
   return (
-    <Button variant="outlined" value={value} onClick={onClick}>
+    <MainButton variant="outlined" value={value} onClick={onClick}>
       {buttonText}
-    </Button>
+    </MainButton>
   );
 };
 
