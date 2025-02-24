d98b057f3dcc9987d6e56ddbbc44bfc26e27219c
Ryan Bowman <rymbowman@gmail.com>
2025-02-24 12:56:29 -0500

    Create Login.jsx and then add it as an element to in a Route in App.jsx

--- Checkbox-Frontend/src/App.jsx
+++ Checkbox-Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import "./styles/App.css";
+import { Login } from "@mui/icons-material";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
