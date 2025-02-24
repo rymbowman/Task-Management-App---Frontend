33bfd55b13605fc6bc66772babb07ea0069b4818
Ryan Bowman <rymbowman@gmail.com>
2025-02-24 08:58:54 -0500

    Set up initial routing with react-router-dom in App.jsx

--- Checkbox-Frontend/src/App.jsx
+++ Checkbox-Frontend/src/App.jsx
@@ -1,5 +1,10 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes></Routes>
+    </Router>
+  );
 }
 
 export default App;
