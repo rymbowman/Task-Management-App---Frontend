b64bffe9a5458ea5c669bdd097efba400c94e441
Ryan Bowman <rymbowman@gmail.com>
2025-02-24 10:17:14 -0500

    Create logo box for landing page

--- Checkbox-Frontend/src/pages/Landing.jsx
+++ Checkbox-Frontend/src/pages/Landing.jsx
@@ -1,5 +1,22 @@
+import { Box, styled, Typography } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+
+const LogoBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  color: "#98FF98",
+});
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <Box>
+      <LogoBox>
+        <CheckIcon sx={{ fontSize: 100 }} />
+        <Typography variant="h1">Checkbox</Typography>
+      </LogoBox>
+      <Typography variant="h5">Achieve More with Every Checkmark</Typography>
+    </Box>
+  );
 };
 
 export default Landing;
