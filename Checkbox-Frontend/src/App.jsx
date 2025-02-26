ebcfe3dd621e7883074db6a0379bd25e0c56fd98
Ryan Bowman <rymbowman@gmail.com>
2025-02-26 13:49:03 -0500

    Create Register.jsx and add path route to this component in App.jsx

--- Checkbox-Frontend/src/App.jsx
+++ Checkbox-Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import "./styles/App.css";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
