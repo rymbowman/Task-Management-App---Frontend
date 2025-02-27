06cfc5a3fe6b73115d087b75550d6bbe5eeed229
Ryan Bowman <rymbowman@gmail.com>
2025-02-27 11:58:56 -0500

    Add styling to dashboard page main container

--- Checkbox-Frontend/src/pages/Dashboard.jsx
+++ Checkbox-Frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,16 @@
+import { Box, styled } from "@mui/material";
+
+const DashboardContainer = styled(Box)({
+  display: "flex",
+  height: "100vh",
+  width: "100%",
+  justifyContent: "center",
+  alignItems: "center",
+  background: "linear-gradient(135deg, #e0f7fa 0%, #f5f5f5 100%)",
+});
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return <DashboardContainer></DashboardContainer>;
 };
 
 export default Dashboard;
