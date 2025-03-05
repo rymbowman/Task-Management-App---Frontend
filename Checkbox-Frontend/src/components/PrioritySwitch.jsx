0c005abb68801880487d84d3a6c5892931de0532
Ryan Bowman <rymbowman@gmail.com>
2025-03-04 20:58:36 -0500

    Extract PrioritySwitch component from relevant section of CreateTask

--- Checkbox-Frontend/src/components/PrioritySwitch.jsx
+++ Checkbox-Frontend/src/components/PrioritySwitch.jsx
@@ -1,5 +1,36 @@
-const PrioritySwitch = () => {
-  return <div>PrioritySwitch</div>;
+import { Collapse } from "@mui/material";
+import InputBox from "./InputBox";
+import SwitchBtn from "./SwitchBtn";
+import PriorityHighIcon from "@mui/icons-material/PriorityHigh";
+import { useState } from "react";
+
+const PrioritySwitch = (createTaskValues, handleChange) => {
+  const [openPriorityInput, setOpenPriorityInput] = useState(false);
+
+  return (
+    <>
+      <SwitchBtn
+        name="priority"
+        value={createTaskValues.priority}
+        onChange={handleChange}
+        icon={<PriorityHighIcon />}
+        labelText="Priority"
+        onClick={() => setOpenPriorityInput(!openPriorityInput)}
+      />
+      <Collapse in={openPriorityInput} timeout="auto">
+        <InputBox
+          label="Priority"
+          name="priority"
+          value={createTaskValues.priority}
+          type="number"
+          onChange={handleChange}
+          slotProps={{
+            input: { min: 0, max: 5 },
+          }}
+        />
+      </Collapse>
+    </>
+  );
 };
 
 export default PrioritySwitch;
