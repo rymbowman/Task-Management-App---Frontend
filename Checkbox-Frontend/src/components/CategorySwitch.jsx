64a8a15e2a08ab0bbadaf29c8320a5423821564c
Ryan Bowman <rymbowman@gmail.com>
2025-03-04 20:55:23 -0500

    Extract CategorySwitch component from relevant section of CreateTask

--- Checkbox-Frontend/src/components/CategorySwitch.jsx
+++ Checkbox-Frontend/src/components/CategorySwitch.jsx
@@ -1,5 +1,34 @@
-const CategorySwitch = () => {
-  return <div>CategorySwitch</div>;
+import { Collapse } from "@mui/material";
+import SwitchBtn from "./SwitchBtn";
+import InputBox from "./InputBox";
+import CategoryIcon from "@mui/icons-material/Category";
+import { useState } from "react";
+
+const CategorySwitch = (createTaskValues, handleChange) => {
+  const [openCategoryInput, setOpenCategoryInput] = useState(false);
+
+  return (
+    <>
+      <SwitchBtn
+        name="category"
+        value={createTaskValues.category}
+        onChange={handleChange}
+        icon={<CategoryIcon />}
+        labelText="Category"
+        onClick={() => setOpenCategoryInput(!openCategoryInput)}
+      />
+      <Collapse in={openCategoryInput} timeout="auto">
+        <InputBox
+          label="Category"
+          name="category"
+          value={createTaskValues.category}
+          type="text"
+          onChange={handleChange}
+        />
+      </Collapse>
+      ;
+    </>
+  );
 };
 
 export default CategorySwitch;
