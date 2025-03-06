3166c20490fb497d413b2f545cdd6557e3be4838
Ryan Bowman <rymbowman@gmail.com>
2025-03-05 20:31:30 -0500

    Extract StepsSwitch component from relevant section of CreateTask

--- Checkbox-Frontend/src/components/StepsSwitch.jsx
+++ Checkbox-Frontend/src/components/StepsSwitch.jsx
@@ -1,5 +1,91 @@
-const StepsSwitch = () => {
-  return <div>StepsSwitch</div>;
+import {
+  Box,
+  Button,
+  Collapse,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  styled,
+  Typography,
+} from "@mui/material";
+import InputBox from "./InputBox";
+import SwitchBtn from "./SwitchBtn";
+import { useState } from "react";
+import ChecklistIcon from "@mui/icons-material/Checklist";
+import DeleteIcon from "@mui/icons-material/Delete";
+import PropTypes from "prop-types";
+
+const AddStep = styled(Box)({
+  display: "flex",
+  gap: "10px",
+  alignItems: "center",
+});
+
+const StepsSwitch = ({ createTaskValues, handleChange }) => {
+  const [openStepsInput, setOpenStepsInput] = useState(false);
+  const [newStep, setNewStep] = useState("");
+
+  return (
+    <>
+      <SwitchBtn
+        name="steps"
+        value={createTaskValues.steps}
+        onChange={handleChange}
+        icon={<ChecklistIcon />}
+        labelText="Steps"
+        onClick={() => setOpenStepsInput(!openStepsInput)}
+      />
+      <Collapse in={openStepsInput} timeout="auto">
+        <List
+          sx={{
+            width: "100%",
+          }}
+        >
+          {Array.isArray(createTaskValues.steps) &&
+          createTaskValues.steps.length > 0 ? (
+            createTaskValues.steps.map((step, index) => (
+              <ListItem
+                key={index}
+                secondaryAction={
+                  <IconButton
+                    onClick={() => handleDeleteStep(index)}
+                    color="primary"
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                }
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <ListItemText primary={`- ${step}`} />
+              </ListItem>
+            ))
+          ) : (
+            <Typography variant="body2" color="textSecondary">
+              No steps added
+            </Typography>
+          )}
+        </List>
+        <AddStep>
+          <InputBox
+            label="New Step"
+            value={newStep}
+            onChange={(e) => setNewStep(e.target.value)}
+          />
+          <Button onClick={handleAddStep} variant="contained" color="primary">
+            Add
+          </Button>
+        </AddStep>
+      </Collapse>
+    </>
+  );
 };
 
+StepsSwitch.propTypes = {
+  createTaskValues: PropTypes.object.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
 export default StepsSwitch;
