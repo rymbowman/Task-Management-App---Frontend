3a0f0b4ea0a3330532b4d545271d4ac6993ac2c5
Ryan Bowman <rymbowman@gmail.com>
2025-03-05 20:17:49 -0500

    Extract NotesSwitch component from relevant section of CreateTask

--- Checkbox-Frontend/src/components/NotesSwitch.jsx
+++ Checkbox-Frontend/src/components/NotesSwitch.jsx
@@ -1,5 +1,39 @@
-const NotesSwitch = () => {
-  return <div>NotesSwitch</div>;
-};
+import { Collapse } from "@mui/material";
+import InputBox from "./InputBox";
+import { useState } from "react";
+import SwitchBtn from "./SwitchBtn";
+import NotesIcon from "@mui/icons-material/Notes";
+import PropTypes from "prop-types";
 
+const NotesSwitch = ({ createTaskValues, handleChange }) => {
+  const [openNotesInput, setOpenNotesInput] = useState(false);
+
+  return (
+    <>
+      <SwitchBtn
+        name="notes"
+        value={createTaskValues.notes}
+        onChange={handleChange}
+        icon={<NotesIcon />}
+        labelText="Notes"
+        onClick={() => setOpenNotesInput(!openNotesInput)}
+      />
+      <Collapse in={openNotesInput} timeout="auto">
+        <InputBox
+          label="Notes"
+          name="notes"
+          value={createTaskValues.notes}
+          type="text"
+          onChange={handleChange}
+          multiline={true}
+          rows={4}
+        />
+      </Collapse>
+    </>
+  );
+};
+NotesSwitch.propTypes = {
+  createTaskValues: PropTypes.object.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
 export default NotesSwitch;
