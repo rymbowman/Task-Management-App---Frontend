dfce39aff9197074333f4881ca84b2d8d50edc8b
Ryan Bowman <rymbowman@gmail.com>
2025-03-05 20:34:08 -0500

    Create date component that can pass as a switch for both DueDate and Reminder Switch

--- Checkbox-Frontend/src/components/DateSwitch.jsx
+++ Checkbox-Frontend/src/components/DateSwitch.jsx
@@ -1,5 +1,53 @@
-const DateSwitch = () => {
-  return <div>DateSwitch</div>;
+import { Box, Collapse } from "@mui/material";
+import { DateField, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import SwitchBtn from "./SwitchBtn";
+import dayjs from "dayjs";
+import PropTypes from "prop-types";
+
+const DateSwitch = ({
+  name,
+  createTaskValues,
+  handleChange,
+  labelText,
+  setOpenInput,
+  openInput,
+  iconImage,
+  label,
+}) => {
+  return (
+    <>
+      <SwitchBtn
+        name={name}
+        value={createTaskValues ? { createTaskValues } : ""}
+        onChange={handleChange}
+        icon={iconImage}
+        labelText={labelText}
+        onClick={() => setOpenInput(!{ openInput })}
+      />
+      <Collapse in={openInput} timeout="auto">
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <Box components={["DateField"]}>
+            <DateField
+              label={label}
+              value={createTaskValues ? dayjs({ createTaskValues }) : null}
+              onChange={(date) => handleDateChange({ name }, date)}
+            />
+          </Box>
+        </LocalizationProvider>
+      </Collapse>
+    </>
+  );
 };
 
+DateSwitch.propTypes = {
+  name: PropTypes.string.isRequired,
+  createTaskValues: PropTypes.object.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  labelText: PropTypes.string.isRequired,
+  setOpenInput: PropTypes.func.isRequired,
+  openInput: PropTypes.bool.isRequired,
+  iconImage: PropTypes.element.isRequired,
+  label: PropTypes.string.isRequired,
+};
 export default DateSwitch;
