60ad5d9f45d2bb686ba215647c26ac4f9cc7c43c
Ryan Bowman <rymbowman@gmail.com>
2025-03-10 18:30:27 -0400

    Update content and styling of Navbar

--- Checkbox-Frontend/src/layouts/Navbar.jsx
+++ Checkbox-Frontend/src/layouts/Navbar.jsx
@@ -2,154 +2,71 @@ import {
   AppBar,
   Avatar,
   Box,
-  Button,
-  Container,
   IconButton,
-  Menu,
-  MenuItem,
+  styled,
   Toolbar,
-  Tooltip,
   Typography,
 } from "@mui/material";
-import { useState } from "react";
-const pages = ["Products", "Pricing", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+import CheckIcon from "@mui/icons-material/Check";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const Nav = styled(AppBar)({
+  backgroundColor: "transparent",
+  boxShadow: "none",
+});
+
+const NavToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+  padding: "0 20px",
+});
+
+const Logo = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  color: "#8FBC8F",
+  gap: "5px",
+});
+
+const IconBtn = styled(IconButton)({
+  "&:hover": {
+    backgroundColor: "transparent",
+  },
+});
+
+const MenuBtn = styled(MenuIcon)({
+  fontSize: 30,
+  color: "#8FBC8F",
+  "&:hover": {
+    color: "#2E8B57",
+  },
+});
+
+const ProfileAvatar = styled(Avatar)({
+  backgroundColor: "#8FBC8F",
+  "&:hover": {
+    backgroundColor: "#2E8B57",
+  },
+});
 
 const Navbar = () => {
-  const [anchorElNav, setAnchorElNav] = useState(null);
-  const [anchorElUser, setAnchorElUser] = useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            ></IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: "block", md: "none" } }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography sx={{ textAlign: "center" }}>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography sx={{ textAlign: "center" }}>
-                    {setting}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <Nav position="static">
+      <NavToolbar>
+        <Box sx={{ display: "flex", alignItems: "center", gap: "20px" }}>
+          <IconBtn>
+            <MenuBtn />
+          </IconBtn>
+          <Logo>
+            <CheckIcon sx={{ fontSize: 40 }} />
+            <Typography variant="h4">Checkbox</Typography>
+          </Logo>
+        </Box>
+        <IconBtn>
+          <ProfileAvatar></ProfileAvatar>
+        </IconBtn>
+      </NavToolbar>
+    </Nav>
   );
 };
 
