d400ef2d01321abc84a2e8d0a9b847a6838cabb9
Ryan Bowman <rymbowman@gmail.com>
2025-03-19 14:23:12 -0400

    Add due date and edit styling for each task

--- Checkbox-Frontend/src/components/TaskContainer.jsx
+++ Checkbox-Frontend/src/components/TaskContainer.jsx
@@ -229,13 +229,39 @@ const TaskContainer = () => {
                 />
               </Tooltip>
               <ListItemText primary={task.title} />
-              <MobileStepper
-                variant="progress"
-                steps={task.details.steps.length + 1}
-                position="static"
-                activeStep={completedSteps[task.id]?.length || 0}
-                sx={{ maxWidth: 400, flexGrow: 1 }}
-              />
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <ListItemText
+                  primary={`Due: ${task.details.dueDate}`}
+                  sx={{
+                    textAlign: "right",
+                    color: "gray",
+                    fontSize: ".875rem",
+                  }}
+                />
+                <MobileStepper
+                  variant="progress"
+                  steps={task.details.steps.length + 1}
+                  position="static"
+                  activeStep={completedSteps[task.id]?.length || 0}
+                  sx={{ maxWidth: 200, flexGrow: 1 }}
+                  slotProps={{
+                    root: {
+                      sx: {
+                        backgroundColor: "transparent",
+                        "& .MuiMobileStepper-progress": {
+                          width: "100%",
+                        },
+                      },
+                    },
+                  }}
+                />
+              </Box>
+
               {openTasks.includes(task.id) ? (
                 <IconButton onClick={() => handleOpenDetails(task.id)}>
                   <ExpandLess />
