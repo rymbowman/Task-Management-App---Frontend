3f775ba4500a96a66b2aa6ce07659ec06eb74252
Ryan Bowman <rymbowman@gmail.com>
2025-03-21 09:29:27 -0400

    Transfer all individual task syntax from original TaskContainer component and replace the dropdown syntax with the TaskDetailsDropdown component

---
+++ Checkbox-Frontend/src/components/task-container/IndividualTask.jsx
@@ -0,0 +1,126 @@
+import {
+  Box,
+  Checkbox,
+  IconButton,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  MobileStepper,
+  Tooltip,
+} from "@mui/material";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import { handleCompletedTask, handleOpenDetails } from "./taskContainerHelpers";
+import PropTypes from "prop-types";
+import TaskDetailsDropdown from "./TaskDetailsDropdown";
+
+const IndividualTask = ({
+  task,
+  completedTasks,
+  setCompletedTasks,
+  completedSteps,
+  setCompletedSteps,
+  openTasks,
+  setOpenTasks,
+}) => {
+  const currentDate = new Date().toISOString().split("T")[0];
+  return (
+    <ListItem
+      key={task.id}
+      disablePadding
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        borderRadius: "8px",
+        border:
+          task.details.dueDate < currentDate
+            ? "2px solid red"
+            : "1px solid rgba(0, 0, 0, 0.1)",
+        backgroundColor:
+          task.details.dueDate < currentDate
+            ? "rgba(255, 0, 0, 0.1)"
+            : "inherit",
+      }}
+    >
+      <ListItemButton value={task.id} sx={{ width: "100%" }} disableRipple>
+        <Tooltip title="Task Completed?" placement="bottom">
+          <Checkbox
+            edge="start"
+            value={task.id}
+            checked={completedTasks.includes(task.id) ? true : false}
+            onChange={(e) =>
+              handleCompletedTask(e, completedTasks, setCompletedTasks)
+            }
+          />
+        </Tooltip>
+        <ListItemText primary={task.title} />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <ListItemText
+            primary={`Due: ${task.details.dueDate}`}
+            sx={{
+              textAlign: "right",
+              color: "gray",
+              fontSize: ".875rem",
+            }}
+          />
+          <MobileStepper
+            variant="progress"
+            steps={task.details.steps.length + 1}
+            position="static"
+            activeStep={completedSteps[task.id]?.length || 0}
+            sx={{ maxWidth: 200, flexGrow: 1 }}
+            slotProps={{
+              root: {
+                sx: {
+                  backgroundColor: "transparent",
+                  "& .MuiMobileStepper-progress": {
+                    width: "100%",
+                  },
+                },
+              },
+            }}
+          />
+        </Box>
+
+        {openTasks.includes(task.id) ? (
+          <IconButton
+            onClick={() => handleOpenDetails(task.id, openTasks, setOpenTasks)}
+          >
+            <ExpandLess />
+          </IconButton>
+        ) : (
+          <IconButton
+            onClick={() => handleOpenDetails(task.id, openTasks, setOpenTasks)}
+            value={task.id}
+          >
+            <ExpandMore />
+          </IconButton>
+        )}
+      </ListItemButton>
+      <TaskDetailsDropdown
+        task={task}
+        openTasks={openTasks}
+        completedSteps={completedSteps}
+        setCompletedSteps={setCompletedSteps}
+      />
+    </ListItem>
+  );
+};
+
+IndividualTask.propTypes = {
+  task: PropTypes.object.isRequired,
+  completedTasks: PropTypes.array.isRequired,
+  setCompletedTasks: PropTypes.func.isRequired,
+  completedSteps: PropTypes.object.isRequired,
+  setCompletedSteps: PropTypes.func.isRequired,
+  openTasks: PropTypes.array.isRequired,
+  setOpenTasks: PropTypes.func.isRequired,
+};
+
+export default IndividualTask;
